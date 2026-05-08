0fc3febc01c6c32e3c5db432b63dd014ba12336d
galaders <kjetil.engvold@outlook.com>
2026-05-08 12:31:31 +0200

    push

---
+++ frontend/src/components/CategoryPage.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import client from "../helpers/client"
+import ProductCard from "./ProductCard"
+
+export default function CategoryPage() {
+    const parameters = useParams()
+    const [category, setCategory] = useState(null)
+
+    useEffect(() => {
+        async function fetchCategory(slug) {
+            const tempCategory = await client.fetch(`*[_type == 'category' && slug.current == $slug]{
+                _id, categoryname, "catProds": *[_type == 'product' && references(^._id)]{..., "imageURL": productimage.asset->url}
+        }`, {slug})
+            setCategory(tempCategory[0])
+        }
+
+        fetchCategory(parameters.slug)
+    }, [parameters])
+
+    console.log(parameters)
+    console.log("Category", category)
+
+
+    return (
+        <>
+            <h1>{category?.categoryname}</h1>
+            {category?.catProds.map((p, index) => <ProductCard key={index} p={p} />)}
+        </>
+    )
+}
